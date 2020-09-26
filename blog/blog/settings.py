4728e63def1937add8851a3f0d817425eeea9b33
zxq <zxq1547080601@163.com>
2020-09-26 22:16:28 +0800

    '配置静态资源'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# 配置静态资源文件路径
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
